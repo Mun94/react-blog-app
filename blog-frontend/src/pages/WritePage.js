1092bb817d26e380ed2fbbe0ebf008c044ce9d8c
Mun94 <seongwoo720@gmail.com>
2020-12-13 22:45:20 +0900

    DONE | 포스트 작성/취소 버튼 생성

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Editor from '../components/write/Editor.js';
 import TagBox from '../components/write/TagBox.js';
 import Responsive from '../components/common/Responsive.js';
+import WriteActionButtons from '../components/write/WriteActionButton.js';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
